cda1b2d33ad6f67640750d0c4d7d31fc466eed28
BrilAlex <brilalex.it@yandex.ru>
2022-08-12 16:43:04 +0300

    Github Pages added

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const rerenderApp = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App/>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>,
     document.getElementById('root')
   );
